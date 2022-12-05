2f235a679cdf12317d1344a0005f4d66f37a19bb
Quan666 <i@rori.Email>
2022-12-05 18:14:54 +0800

    fix: chatgpt 限制session-token刷新次数

--- elfchatbot/plugins/ELFChatBot/ChatBotApi/chatgpt.py
+++ elfchatbot/plugins/ELFChatBot/ChatBotApi/chatgpt.py
@@ -1,3 +1,4 @@
+import time
 from typing import Optional
 import httpx
 import json
@@ -15,6 +16,7 @@ class ChatGPT:
     authorization: Optional[str] = None
     session_token: str
     proxy: Optional[httpx.Proxy] = None
+    last_refresh = 0
 
     conversation_id: Optional[str]
     parent_message_id: str
@@ -128,14 +130,20 @@ class ChatGPT:
 
     @classmethod
     async def refresh_session(cls):
+        if cls.last_refresh + 60 * 30 > time.time():
+            return
+        cls.last_refresh = time.time()
+
         s = httpx.AsyncClient(proxies=cls.proxy)
         s.cookies.set("__Secure-next-auth.session-token", cls.session_token)
         response = await s.get("https://chat.openai.com/api/auth/session")
         try:
-            cls.session_token = response.cookies.get("__Secure-next-auth.session-token")
-            cls.authorization = response.json()["accessToken"]
+            cls.session_token = response.cookies.get(
+                "__Secure-next-auth.session-token")
+            cls.authorization = response.json().get("accessToken", cls.authorization)
         except Exception as e:
-            raise Exception("Error refreshing session: " + str(e))
+            raise Exception(
+                f"Error refreshing session: {str(e)} {response.text}")
 
 
 if __name__ == "__main__":
@@ -144,10 +152,12 @@ if __name__ == "__main__":
     async def main():
         chat1 = ChatGPT(proxy="127.0.0.1:7890")
         ChatGPT.global_init(
-            session_token="",
+            session_token="eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..3NLgpPg2AtMrszf9.6PGxBZ-GRJKvAYrq1Fy1XQ9irz31x_pwWYbmxaNxuoCqHQA2Tz6MbVmNQvSts8jF64znSI5jU_XtFz0It-tDhnoTjoODCklGLwNTGRn5lfFdBHVHXHQpAM1hiit-CZ6RZfeOmeONUixP98zDzxwXS6kmyQKqnQk0Tq2w2B99Ol-e6_ASan9mW6lEL86x1VaRkfho6Rkb8jQg283uj934Q3alMU-ZNrG6wZvx-TaMJnAuDqddCJ-VYmbYsPeZX-1nrCWpCHsxszl2J8GQRnjVXgow97lez--987SoCbvfREPqgtyH_qWjrQSgYPSq1TAdH4aeSDeR1ozByyUMl3s-B6EAJGYqc9N2asVrFgx9GzurGSNZu2EbjWl1YpNu9R_x-UvJ6HreoS0oGWZkDWYq0fx2LcwQ88g8Xwr-w1XOgU0hqwAH_s2GAmn9reCyYNcbbJfNHQMfTwkQCfFwqpYXifOYLGb4Ed2cMIWMY84yoGWzlFMqDRAfcnL2MKlAO6iEZhVbEoZXh1-_jet0yVPN3ux9wnA4014492ZK4vh7swn9Y4v966aiatiglLYCNfOpjAwCAzGhyUv45w6gq60zByjiYFj4NcOVHHIVlfgLGDfEzbPS202NXWB2wif0ptpch68smEQwIGrBMRD4Ad-2kqCHBsESEMNRKNAU_ZMWBGjSHo9-pF4Tx6Wbeaqxn68SK6Z5jRnR5SQZ0zJKg0z7awqjVz6zUodMT8pWYgsbznZYJL5w6FuS_tm_CRxo1AWmGdMqLeQdEnScvLAbaBLulgv1WOihOMb4aLzYkd1OEnfXQVZWN2OPhfM0jRtOcG_Peq2taDG6jCjRWyvcf4y0AAzPbuoIsKFSbb_brkg9IjGY8JxgFw1oFYz8_tS2ez9cWfDgsl0ZjyZLvpdAELYrZQ8UlN5Zvx15q8HZuTSlW0i2z7D1RaM1weD6RXDagrD81lVa46TUts8QFRpyk9qieGwvLU_K2I8tq-slueVbSnBIocDuXbuXLQYvMUCPJb4aEK7tvpoYRzGYxKdSpnMZEMdxmBtDP5PO4tUph0IswW3dN5aHZW-Z6miry135KqsPUGLqNpZcqptmsQ4pTCyVK2c79aAhkQZfbm8kbe_cAi6cA9LuMwvQhLA4T29I38_5BYeSQBKw3w_vy-5zLSAMVeA7QBNG13TFLnxA6eyArxdXhgNFT5gRbycdNNPz-U2dvn-icAVdnxkadaV0Dnu2ccAnLqidZkcFjbLqacSaL-iRq4nuB_ITWqPphnvQruWtkHnXNQXUdVzGLvzO8O9ZYu5lDWuVRIdSXnqpr4s_U_D-cXcUBw01d9Rr6MpwSSk3CCHYwWLc_hrww6_I2MsCLMj7Cc2ewrN9FG9vLTZzkjz0ryAjwRcWhgtYjUGXAwkFt6Vqt5_EMbu0mbcUHMqKZJ4ORzI3eC63VVbjKPwAV0uY6T1DX0wLc8bZbseNp9V2By_TyIVTZ4TrZWpkDjSxErjFABU_xoCmGS5lZaie9UtCMuYHN4KYTO6IFtHAqZrOyKkv-2bcIeqsbLc4f5_tek-nF4z1SaFEzOUDikdS6jgx3TzJnYmpIe2CSkBTuF5M4pqf-NHsUNLQny168LhY0aQ0S7giabHQBirokk7tGnCXggt7i-0qP0wXuzxl3rLOwOh9wlyOz5YZlt5zlEFNq5TKRMT2b5I9a3C6uSUc8atXBVxPmDaD7SvLG5B8oUlgZ7lyA3vm7obBSS7RjkecPD1W08vi9lfQKukncn_TlHXSdCdnWQ0NZlNq49LsG4dkTWFkqrhdy7fQ3WKY8PntGXmKxUC6jVJqAQNnBetdsClmtgsik1K4GXVvrZnGgmlhYT9onilq9rUenIkM3TQw77y13diYGp1vPY97XGasX8yoLW-o6LdOUjOkq5r1VdHTGbfoOfvLvlDh4mfuxO6CSdVD_xWbF-AfDLNLImmEDO2GFSySyrJ9iBZQEFUauRzckXwdx_bdzvWs_11jIfgx6KyuDO60ta4UVe1_Z9gVUk8aOWe8TDs-NA13Qpo4b26-Akqq1pQIgMskbUX5C1RR4k61Pc6Ob4cP7pmM_Q1dDmNJmVPFVkLOk8mK7q04MRZs1_6FUeMD37xPxhDVNS5H3Fz4Rckv-Xtg8yGfQJjOszH8dsgOqOiLRSH07p5pS5-e9MwMZvaDYe75DGm1AWa25cFdD8Cle54D3xG0Ouv519KtWS88SbjyTySynJXQFqn0BnTba_xZAQFoiZjOP8DHD3uMlQ2fT_j3MHOxYBVZExhTNIE.7LcW3_W2_u6QEODUCJg02Q",
         )
 
         chat2 = ChatGPT(proxy="127.0.0.1:7890")
+        await ChatGPT.refresh_session()
+        await ChatGPT.refresh_session()
         await ChatGPT.refresh_session()
         print(chat1.authorization)
         print(chat2.authorization)
